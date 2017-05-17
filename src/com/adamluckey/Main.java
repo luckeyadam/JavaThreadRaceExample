3644b4706451b0c01fcd7594cf056a198bccff17
Adam Luckey <adam.luckey@jivesoftware.com>
2017-05-17 10:31:10 -0700

    initial commit

---
+++ src/com/adamluckey/Main.java
@@ -0,0 +1,57 @@
+package com.adamluckey;
+
+/**
+ * Race condition example
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Countdown countdown = new Countdown();
+
+        CountdownThread thread1 = new CountdownThread(countdown);
+        thread1.setName("Thread 1");
+        CountdownThread thread2 = new CountdownThread(countdown);
+        thread2.setName("Thread 2");
+
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class Countdown {
+
+    private int i;
+    public void doCountDown() {
+        String color;
+        switch (Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ThreadColor.ANSI_CYAN;
+                break;
+            case "Thread 2":
+                color = ThreadColor.ANSI_PURPLE;
+                break;
+            default:
+                color = ThreadColor.ANSI_GREEN;
+        }
+
+        // changing i from a local variable ex: for (int i = 10; i > 0; i--) allows each thread
+        // to have an instance of i to keep integers printing individual values for each thread
+        // the current iteration may cause singular and duplicate results depending on when
+        // each thread is suspended such as during the decrement or the println, i.e race condition
+        for (i = 10; i > 0; i--) {
+            System.out.println(color + Thread.currentThread().getName() + ": i = " + i);
+        }
+    }
+}
+
+class CountdownThread extends Thread {
+    private Countdown threadCountdown;
+
+    public CountdownThread(Countdown countdown) {
+        threadCountdown = countdown;
+    }
+
+    public void run() {
+        threadCountdown.doCountDown();
+    }
+}
